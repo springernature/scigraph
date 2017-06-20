eac1d6158c8f07cb433368eeaddd0c7f5f307c99
Hilverd Reker <me@hilverd.com>
2017-06-20 09:34:16 +0100

    Improve build-and-run script

--- 2017/hackday-2017-06-23/examples/apps/java/build-and-run.sh
+++ 2017/hackday-2017-06-23/examples/apps/java/build-and-run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-echo "Building application"
+set -e
+
 mvn clean compile assembly:single
 
-echo "Executing application"
 java -jar target/java-client-graphdb-1.0-SNAPSHOT-jar-with-dependencies.jar
